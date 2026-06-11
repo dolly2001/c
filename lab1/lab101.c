310fc77c98ee1ac1dfbcef0d3d5fc12b8166be09
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-11 11:51:19 +0000

    Basic Template

---
+++ lab1/lab101.c
@@ -0,0 +1,16 @@
+// Header files contain declarations of pre-written functions.
+#include<stdio.h>
+#include<math.h>
+
+// This is the main function
+// Program execution starts from the main() function.
+int main(){
+    // printf() is a pre-written function which let you print anything on console.
+    // printf() displays text on the console (standard output).
+    printf("HELLO World");
+
+    // It tells the operating system how the program finished.
+    // The return value of main() is sent to the operating system.
+    // 0 = success, non-zero = error.
+    return 0;
+}
