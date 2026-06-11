6e7a71151b7d9dbc49f1e913782ba6e5871692bd
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-11 12:03:28 +0000

    adding function() file

---
+++ lab1/lab102.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// Function -> A function is a block of code that help us avoid repeating the same code multiple times.
+
+void getHelloWorld(){
+    printf("Hello World");
+}
+
+int main(){
+    // Function call 
+    getHelloWorld();
+    getHelloWorld();
+    return 0;
+}
