8c3c9f3549acb583a913c1013003788e887dcade
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-11 12:47:35 +0000

    how to take input

---
+++ lab1/lab104.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+    // How to take input from user.
+    int age;
+    printf("Enter your age :: ");
+    scanf("%d", &age);
+
+    printf("%d", age);
+
+    return 0;
+}
