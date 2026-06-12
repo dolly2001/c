a48f8a5be0b638ed43b144976877190894440657
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-12 08:02:26 +0000

    password checker

---
+++ lab1/miniproject.c
@@ -0,0 +1,23 @@
+// Password Cheaker!
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char password[50] = "Piyush";
+char input[50];
+printf("Enter password : ");
+scanf("%s", input);
+
+if(strcmp(password, input) == 0){
+    printf("Correct Password!!");
+}
+else{
+    printf("Incorrect Password!!!");
+}
+return 0;
+}
+
+/*
+Note -> if you compare like this if(password == input), you're comparing address of password == address of input insted of this one should use string comparision function. 
+strcmp(password, input) = 0
+*/
