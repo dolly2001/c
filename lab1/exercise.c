9c50738ae708b5ce967c55de1361973d0cb2b3c3
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-12 06:25:00 +0000

    String

---
+++ lab1/exercise.c
@@ -0,0 +1,11 @@
+// Write a program to take name as input from user ?
+
+#include <stdio.h>
+int main()
+{
+char name[50];
+printf("Enter your good name (without any space) :: ");
+scanf("%s", name);
+printf("%s", name);
+return 0;
+}
---
+++ lab1/lab105.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+// how to print an string.
+printf("String printing");
+char name[] = "Piyush";
+printf("%s", name);
+return 0;
+}
+
+
+// que. WAP to take name as input from user ?
