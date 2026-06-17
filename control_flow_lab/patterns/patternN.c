4418fbec27d497ab5470d019f948fde3c36345c9
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-17 15:00:21 +0000

    N Pattern

---
+++ control_flow_lab/patterns/patternN.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int row;
+printf("Enter row :: ");
+scanf("%d", &row);
+
+for(int i = 1; i <= row; i++)
+{
+	for(int j = 1; j <= i; j++)
+	{
+		printf("%d", i);
+	}
+putchar(10);
+}
+return 0;
+}
