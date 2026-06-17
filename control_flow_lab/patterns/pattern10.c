10934f3ea14c9e9e70d06bd0c2508f701c2fd0a8
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-17 16:37:38 +0000

    bit of trick

---
+++ control_flow_lab/patterns/pattern10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int row;
+printf("Enter row :: ");
+scanf("%d", &row);
+
+for(int i = 1; i <= row; i++)
+{
+	for(int j = 1; j <= row-i; j++)
+	{
+		printf(" ");
+	}
+	for(int k = 1; k <= i; k++)
+	{
+		printf("* ");
+	}
+	putchar(10);
+}
+return 0;
+}
