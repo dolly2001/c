58fda912e437c5c6fcfdcf7736e75872f0e69bed
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-17 15:08:59 +0000

    pattern 2

---
+++ control_flow_lab/patterns/pattern02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int row;
+printf("Enter rows :: ");
+scanf("%d", &row);
+
+for(int i = 1; i <= row; i++)
+{
+	for(int j = 1; j <= i; j++)
+	{
+		printf(" * ");
+	}
+	putchar(10);
+}
+
+return 0;
+
+}
