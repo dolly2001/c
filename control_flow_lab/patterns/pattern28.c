00b963c898d5b615552948e0ac21bec1fba18fc3
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-19 09:41:20 +0000

    Pattern 28

---
+++ control_flow_lab/patterns/pattern28.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+int row;
+printf("Enter row :: ");
+scanf("%d", &row);
+
+for(int i = 1; i <= row; i++)
+{
+	for(int j = 1; j <= row-i; j++)
+	{
+		printf(" ");
+	}
+	for(int k = 1; k <= i; k++)
+	{
+		printf("* ");
+	}
+	putchar(10);
+}
+for(int i = 1; i < row; i++)
+{
+	for(int j = 1; j <= i; j++)
+	{
+		printf(" ");
+	}
+	for(int k = 1; k <= row-i; k++)
+	{
+		printf("* ");
+	}
+	putchar(10);
+}
+return 0;
+}
