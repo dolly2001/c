7a1bb3c171ada25c4d091db1805a3654147185ed
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-22 06:01:38 +0000

    pattern13

---
+++ control_flow_lab/patterns/pattern13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+int row;
+printf("Enter row :: ");
+scanf("%d", &row);
+
+for(int i = 1; i <= row; i++)
+{
+	for(int j = 1; j<= row-i; j++)
+	{
+		printf(" ");
+	}
+	for(int k = 1; k <= (2*i)-1; k++)
+	{
+		if(k==1 || k==(2*i)-1 || i==row)
+		{
+			printf("*");
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+	putchar(10);
+}
+
+return 0;
+
+}
