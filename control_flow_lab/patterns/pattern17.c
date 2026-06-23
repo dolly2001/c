8c88bf46b76ccf8fa9f27167fc38793aa6977794
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-23 21:36:21 +0000

    17th pattern

---
+++ control_flow_lab/patterns/pattern17.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+int row;
+printf("Enter row :: ");
+scanf("%d", &row);
+
+for(int i = 1; i <= row; i++)
+{
+	for(int j = 1; j <= row-i; j++)
+	{
+		printf(" ");
+	}
+	for(int k = i; k >= 1; k--)
+	{
+		printf("%d", k);
+	}
+	for(int l = 2; l <= i; l++)
+	{
+		printf("%d", l);
+	}
+	putchar(10);
+}
+for(int i = row-1; i >= 1; i--)
+{
+	for(int j = 1; j <= row-i; j++)
+	{
+		printf(" ");
+	}
+	for(int k = i; k >= 1; k--)
+	{
+		printf("%d", k);
+	}
+	for(int l = 2; l <= i; l++)
+	{
+		printf("%d", l);
+	}
+	putchar(10);
+} 
+return 0;
+}
