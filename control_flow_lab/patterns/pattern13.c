0207385643cecfacd4c85cf9a6f522251e6d4416
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-23 04:25:46 +0000

    copy

--- control_flow_lab/patterns/pattern13.c
+++ control_flow_lab/patterns/pattern13.c
@@ -7,13 +7,13 @@ scanf("%d", &row);
 
 for(int i = 1; i <= row; i++)
 {
-	for(int j = 1; j<= row-i; j++)
+	for(int j = 1; j <= row-i; j++)
 	{
 		printf(" ");
 	}
 	for(int k = 1; k <= (2*i)-1; k++)
 	{
-		if(k==1 || k==(2*i)-1 || i==row)
+		if(k==1|| k == (2*i)-1|| i==row)
 		{
 			printf("*");
 		}
