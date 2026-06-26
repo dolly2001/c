db11008afadb8ce082a8b607879f588812db7cc5
dolly2001 <piyushsolanki18dec@gmail.com>
2026-06-26 07:58:38 +0000

    1st trick question

---
+++ control_flow_lab/lab1/que1.c
@@ -0,0 +1,15 @@
+/* Wait and think about the output
+	i++
+	first use the value
+	then increase the value
+*/
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	if(i++)
+	{
+		printf("Hii");
+	}
+return 0;
+}
